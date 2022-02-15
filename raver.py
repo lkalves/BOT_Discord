86e978071a1a5a854bad3e261eaf1c297eb58ff8
Italo <italo.gama@Italos-MacBook-Pro.local>
2022-02-15 11:09:58 -0300

    Adicionando mutex no onmessage para ele ler uma mensagem de cada vez, caso alguem mande varias ao mesmo tempo

--- raver.py
+++ raver.py
@@ -1,16 +1,17 @@
 from discord.ext import commands
 import discord
 import pyttsx3
-
+from multiprocessing import Lock
 
 engine = pyttsx3.init()
 engine.setProperty("voice", "brazil")
 engine.setProperty("rate", 180)
 engine.runAndWait()
 
-AUTHOR = 'LK'
+AUTHOR = 'gamaitalo'
 TOKEN = "OTM4Nzg1ODU4ODU1Nzk2NzY2.YfvWdg.34gNUwxBfwwA8IRsf0YwesolA-o"
 
+mutex = Lock()
 bot = commands.Bot("!")
 
 
@@ -28,14 +29,15 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    if message.channel.name == 'bot' and message.author.name == AUTHOR:
-        if not is_connected(message.author.voice.channel):
-            bot.vc = await message.author.voice.channel.connect()
-        print(f'{message.author}: {message.content}')
-        engine.save_to_file(message.content, 'test.webm')
-        engine.runAndWait()
-        source = await discord.FFmpegOpusAudio.from_probe(executable='D:\\DEV\\Projetos\\Webhook_Discord\\ffmpeg-win64\\bin\\ffmpeg.exe', source='test.webm')
-        bot.vc.play(source)
+    with mutex:
+        if message.channel.name == 'bot' and message.author.name == AUTHOR:
+            if not is_connected(message.author.voice.channel):
+                bot.vc = await message.author.voice.channel.connect()
+            print(f'{message.author}: {message.content}')
+            engine.save_to_file(message.content, 'test.webm')
+            engine.runAndWait()
+            source = await discord.FFmpegOpusAudio.from_probe(executable='D:\\DEV\\Projetos\\Webhook_Discord\\ffmpeg-win64\\bin\\ffmpeg.exe', source='test.webm')
+            bot.vc.play(source)
 
 
 def main():
