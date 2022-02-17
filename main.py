f5199001af4716059759529d83fb278a9d881853
Lucas Alves de Souza <lucasalves007@outlook.com>
2022-02-17 19:50:16 -0300

    Adicionando lista de usuarios permitidos

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from multiprocessing import Lock
 
 engine = pyttsx3.init()
 engine.setProperty("voice", "brazil")
-engine.setProperty("rate", 180)
+engine.setProperty("rate", 170)
 engine.runAndWait()
 
-AUTHOR = 'LK'
+AUTHOR = ['LK', 'Pseudao']
+CANAL = 'bot'
 TOKEN = "OTM4Nzg1ODU4ODU1Nzk2NzY2.YfvWdg.34gNUwxBfwwA8IRsf0YwesolA-o"
 
 mutex = Lock()
@@ -25,19 +26,19 @@ def is_connected(channel):
 
 @bot.event
 async def on_ready():
-    print(f'Botzada do Aloes {bot.user}\nDoubleG *Copyright*')
+    print(f'Botzada {bot.user}\nDoubleG *Copyright*')
 
 
 @bot.event
 async def on_message(message):
     with mutex:
-        if message.channel.name == 'bot' and message.author.name == AUTHOR:
+        if (message.channel.name == CANAL and message.author.name in AUTHOR) or message.channel.name == 'canal-do-alan':
             if not is_connected(message.author.voice.channel):
                 bot.vc = await message.author.voice.channel.connect()
             print(f'{message.author}: {message.content}')
             engine.save_to_file(message.content, 'test.webm')
             engine.runAndWait()
-            source = await discord.FFmpegOpusAudio.from_probe(executable='D:\\DEV\\Projetos\\Webhook_Discord\\ffmpeg-win64\\bin\\ffmpeg.exe', source='test.webm')
+            source = await discord.FFmpegOpusAudio.from_probe(executable='D:\\DEV\\Projetos\\Webhook_Discord\\ffmpeg\\ffmpeg.exe', source='test.webm')
             bot.vc.play(source)
 
 
