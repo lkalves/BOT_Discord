e0ca7cbf4fa41449dee4050a7ce7ad4e7957c0bd
Lucas Alves de Souza <lucasalves007@outlook.com>
2022-02-23 09:58:55 -0300

    Adicionado novos IDs a lista e tratamento para nao cadastrado

--- tools/messageHandler.py
+++ tools/messageHandler.py
@@ -25,7 +25,15 @@ def removeUserID(texto):
     if hasUserID(texto):
         # Metodo que tira o ID
         nome_lista = [('270928429925269518', 'LK'), ('268198611941195776', 'Rafão'),
-                      ('107332797332508672', 'Dashtail'), ('109427358162817024', 'Pseudão')]
+                      ('107332797332508672',
+                       'Dashtail'), ('109427358162817024', 'Pseudão'),
+                      ('112262967101255680',
+                       'Luorhane'), ('115948020360675329', 'Itala'),
+                      ('192460714512744448',
+                       'Matiserguei'), ('112964615335321600', 'Peterola'),
+                      ('114546184193835011',
+                       'Paithias'), ('115948643722330119', 'Gijas'),
+                      ('209833913336463362', 'Aloens')]
 
         a = texto.split('<@!')
         b = a[1].split('>')
@@ -34,6 +42,8 @@ def removeUserID(texto):
         search = dicio.get(id, 'Não encontrado')
         if search != 'Não encontrado':
             result = a[0] + '@'+search + b[1]
+        elif search == 'Não encontrado':
+            result = a[0] + '@desconhecido' + b[1]
         return removeUserID(result)
     else:
         return texto
