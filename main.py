d3dfab9302b963e0f2a3561a3a846d6c1abda52e
Lucas Alves de Souza <lucasalves007@outlook.com>
2022-04-07 19:25:28 -0300

    Feito o tratamento de quem está desconectado
    Tratamento de quem está desconectado de um canal de voz e chama o bot.
    Ajustado o metodo que retira o ID usuario do conteudo de mensagem.

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ dotenv.load_dotenv(dotenv.find_dotenv())
 mutex = Lock()
 bot = commands.Bot("!")
 
+CANAL = os.getenv('CANAL')
+AUTHOR = os.getenv('AUTHOR')
+CANALEXC = os.getenv('CANAL_EXC')
+
 
 def is_connected(channel):
     for voice_client in bot.voice_clients:
@@ -26,20 +30,30 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    channel = message.channel
+    author = message.author
+
     with mutex:
-        if (message.channel.name == (os.getenv('CANAL')) and message.author.name in (os.getenv('AUTHOR'))) or message.channel.name == (os.getenv('CANAL_EXC')):
-            if not is_connected(message.author.voice.channel):
-                bot.vc = await message.author.voice.channel.connect()
-            if is_connected(message.author.voice.channel) and message.content.startswith('!dc'):
+        if (channel.name == CANAL and author.name in AUTHOR) or channel.name == CANALEXC:
+            if not is_connected(author.voice):
+                if author.voice is None:
+                    await channel.send('Você não está conectado em um canal de voz')
+                else:
+                    bot.vc = await author.voice.channel.connect()
+
+            if is_connected(author.voice.channel) and message.content.startswith('!dc'):
                 await bot.vc.disconnect()
-            print(f'{message.author}: {message.content}')
+
+            print(f'{author}: {message.content}')
             verifyUser(message.content)
             removeID = removeUserID(message.content)
+
             tts = gtts.gTTS(removeID, lang='pt', slow=False)
             tts.save("audio/test.mp3")
             source = await discord.FFmpegOpusAudio.from_probe(
                 'audio/test.mp3'
             )
+
             bot.vc.play(source)
 
 
