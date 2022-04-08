7c386d6b15a2c522719e299d72fe0a905eb28349
Lucas Alves de Souza <lucasalves007@outlook.com>
2022-04-08 12:11:47 -0300

    Ajustes de conteudo de mensagem limpo

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from sys import executable
+from time import sleep
 import dotenv
 from discord.ext import commands
 import discord
 from multiprocessing import Lock
-from tools.messageHandler import verifyUser, removeUserID
 import gtts
 import os
+from datetime import datetime
+
+import traceback
+import sys
 
 dotenv.load_dotenv(dotenv.find_dotenv())
 
@@ -15,6 +18,11 @@ bot = commands.Bot("!")
 CANAL = os.getenv('CANAL')
 AUTHOR = os.getenv('AUTHOR')
 CANALEXC = os.getenv('CANAL_EXC')
+
+
+async def remove_file(file_path):
+    sleep(5)
+    os.remove(file_path)
 
 
 def is_connected(channel):
@@ -33,8 +41,13 @@ async def on_ready():
 async def on_message(message):
     channel = message.channel
     author = message.author
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
+
+    print(message.clean_content)
 
     with mutex:
+
         if author.voice is None:
             await channel.send('Você não está conectado em um canal de voz')
 
@@ -42,19 +55,25 @@ async def on_message(message):
             if not is_connected(author.voice.channel):
                 bot.vc = await author.voice.channel.connect()
 
-            if is_connected(author.voice.channel) and message.content.startswith('!dc'):
+            if is_connected(author.voice.channel) and message.clean_content.startswith('!dc'):
                 await bot.vc.disconnect()
+                return
 
-            print(f'{author}: {message.content}')
-            txt = message.content.lower()
-            verifyUser(txt)
-            removeID = removeUserID(message.content)
+            print(f'{author}: {message.clean_content}')
+            txt = message.clean_content.lower()
 
-            tts = gtts.gTTS(removeID, lang='pt', slow=False)
-            tts.save("audio/test.mp3")
-            source = await discord.FFmpegOpusAudio.from_probe('audio/test.mp3')
+            try:
+                aud = f"audio/{timestamp}.mp3"
+                tts = gtts.gTTS(txt, lang='pt', slow=False)
+                tts.save(aud)
+                source = await discord.FFmpegOpusAudio.from_probe(source=f'audio/{timestamp}.mp3', executable='tools/ffmpeg/ffmpeg.exe')
+                bot.vc.play(source)
+                await remove_file(aud)
 
-            bot.vc.play(source)
+            except Exception:
+                ex_type, ex, tb = sys.exc_info()
+                traceback.print_tb(tb)
+                print('Deu pau!')
 
 
 def main():
