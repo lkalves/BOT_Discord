140da245fd6a496b122f244095089ca54f109d0c
Lucas Alves de Souza <lucasalves007@outlook.com>
2022-04-08 19:22:13 -0300

    Retirado o executável do FFMPEG

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import sys
 from time import sleep
 from discord.ext import commands
 from datetime import datetime
-from multiprocessing import Lock
 from mutagen.mp3 import MP3
 
 dotenv.load_dotenv(dotenv.find_dotenv())
@@ -18,8 +17,10 @@ CANAL = os.getenv('CANAL')
 AUTHOR = os.getenv('AUTHOR')
 CANALEXC = os.getenv('CANAL_EXC')
 
+
 async def remove_file(file_path):
     os.remove(file_path)
+
 
 def is_connected(channel):
     for voice_client in bot.voice_clients:
@@ -27,18 +28,19 @@ def is_connected(channel):
             return True
     return False
 
+
 async def reproduce_audio(message):
     channel = message.channel
     author = message.author
     now = datetime.now()
     timestamp = datetime.timestamp(now)
-    
+
     if author.voice is None:
         await channel.send('Você não está conectado em um canal de voz')
 
     if not is_connected(author.voice.channel):
         bot.vc = await author.voice.channel.connect()
-        
+
     if is_connected(author.voice.channel) and message.clean_content.startswith('!dc'):
         await bot.vc.disconnect()
         return
@@ -50,7 +52,7 @@ async def reproduce_audio(message):
         aud = f"audio/{timestamp}.mp3"
         tts = gtts.gTTS(txt, lang='pt', slow=False)
         tts.save(aud)
-        source = await discord.FFmpegOpusAudio.from_probe(source=f'audio/{timestamp}.mp3', executable='tools/ffmpeg/ffmpeg.exe')
+        source = await discord.FFmpegOpusAudio.from_probe(f'audio/{timestamp}.mp3')
         bot.vc.play(source)
         audio = MP3(f"audio/{timestamp}.mp3")
         sleep(audio.info.length)
@@ -61,9 +63,11 @@ async def reproduce_audio(message):
         traceback.print_tb(tb)
         print(f"Exception: {e}")
 
+
 @bot.event
 async def on_ready():
     print(f'Botzada {bot.user}\nDoubleG *Copyright*')
+
 
 @bot.event
 async def on_message(message):
@@ -71,8 +75,10 @@ async def on_message(message):
         print(f"Tem mensagem nova! Mensagem: {message.clean_content}")
         await reproduce_audio(message)
 
+
 def main():
     bot.run(os.getenv('TOKEN'))
-    
+
+
 if __name__ == "__main__":
     main()
